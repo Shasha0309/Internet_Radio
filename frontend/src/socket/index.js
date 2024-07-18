15df91791b7b1a5fd217352fa440ca8fc16fc228
Shasha0309 <sona2002sharma@gmail.com>
2024-07-18 21:43:54 +0530

    Add files via upload

---
+++ frontend/src/socket/index.js
@@ -0,0 +1,12 @@
+import { io } from 'socket.io-client';
+export const socketInit = () => {
+    const options = {
+        'force new connection': true,
+        reconnectionAttempt: 'Infinity',
+        timeout: 10000,
+        transports: ['websocket'],
+    };
+    return io(process.env.REACT_APP_SOCKET_SERVER_URL, options);
+};
+
+
